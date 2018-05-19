9eef840770e075faf5379dfded53eea9d5904377
Jayahari Vavachan <jayahariv88@gmail.com>
2018-05-19 10:27:41 -0700

    refactor: Right Left Enum Added

--- swift/ArrayRotate.swift
+++ swift/ArrayRotate.swift
@@ -1,19 +1,34 @@
 #!/usr/bin/swift
 
+enum RotateSide {
+    case right;
+    case left;
+}
+
 extension Array {
     
     // O(n) - Performance
     // O(n) - Memory
-    func rightRotateNaive(_ noOfTimes: Int) -> [Element] {
+    func rotateNaive(_ noOfTimes: Int, side: RotateSide) -> [Element] {
         var items = [Element]()
-        let times = noOfTimes % count + 1 // since the times is different from index
-        var index = times
+        let times = noOfTimes % count
+        
+        
+        let position = 
+            side == RotateSide.right
+                ? count - times // if noOfTimes is 2 and count is 6, thats means first element will become 6-2 = 4th index item and so on.
+                : times // if noOfTimes is 2, first element will be in 2nd position item nd so on. 
+        
+        // After finishing position to End of array appending, add the remaining items from 0 index to position where we started. 
+
+        var index = position
+
         while index < count {
             items.append(self[index])
             index += 1
         }
         index = 0
-        while index < times {
+        while index < position {
             items.append(self[index])
             index += 1
         }
@@ -32,10 +47,13 @@ extension Array {
      for example, 3 times. you will end up getting
      => 3, 4, 5, 1, 2
      */
-    mutating func rightRotateBubble(_ noOfTimes: Int) {
-        
+    mutating func rotateBubble(_ noOfTimes: Int, side: RotateSide) {
+        let list = 
+            side == RotateSide.right 
+                ? stride(from: self.count - 1, to: 0, by: -1) 
+                : stride(from: 1, to: self.count, by: 1)
         for _ in 1...noOfTimes {
-            for i in stride(from: self.count - 1, to: 0, by: -1) {
+            for i in list {
                 let temp = self[i]
                 self[i] = self[i - 1]
                 self[i - 1] = temp
@@ -44,10 +62,18 @@ extension Array {
     }
 }
 
-let input = [1, 2, 3, 4, 5]
-let resultNaive = input.rightRotateNaive(2)
-print(resultNaive)
+print("\nNaive Rotation")
+let input = [1, 2, 3, 4, 5, 6]
+let rightResultNaive = input.rotateNaive(2, side: .right)
+print("Input is: \(input) Result after Right Rotation of 2 times is: \(rightResultNaive) ")
+let leftResultNaive = input.rotateNaive(2, side: .left)
+print("Input is: \(input) Result after Left Rotation of 2 times is: \(leftResultNaive) ")
 
-var inputBubble = [1, 2, 3, 4, 5]
-inputBubble.rightRotateBubble(2)
-print(inputBubble)
+print("\nBubble Rotation")
+var inputBubble = [1, 2, 3, 4, 5, 6, 7, 8]
+let inputBubbleInput1 = inputBubble
+inputBubble.rotateBubble(3, side: .right)
+print("Input is: \(inputBubbleInput1) Result after Right Bubble Rotation of 3 times is: \(inputBubble) ")
+let inputBubbleInput2 = inputBubble
+inputBubble.rotateBubble(2, side: .left)
+print("Input is: \(inputBubbleInput2) Result after Left Bubble Rotation of 2 times is: \(inputBubble) ")
