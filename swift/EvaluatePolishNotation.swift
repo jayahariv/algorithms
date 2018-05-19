0bb4a161ab5d0408041c429fcac7d796cf92d3c1
Jayahari Vavachan <jayahariv88@gmail.com>
2018-05-19 12:34:41 -0700

    feat: Evaluate Polish Notation
    * evaluate polish notation problem
    * basic stack implementation
    * basic int checking for a given string

---
+++ swift/EvaluatePolishNotation.swift
@@ -0,0 +1,92 @@
+#!/usr/bin/swift
+
+/*
+	Evaluate the value of an arithmetic expression in Reverse Polish Notation. Valid
+	operators are +, -, *, /. Each operand may be an integer or another expression
+
+	// I
+	1, 2, + => 1 + 2 = 3
+	
+	// II
+	1, 2, +, 4, * => (1 + 2) * 4 = 12
+
+	// III
+	1, 2, +, 5, 1, *, + => (1 + 2) + (5 * 1) = 8
+
+	// IV
+	1, 2, 3, +, * => 1 * (2 + 3) = 5
+
+*/
+
+// Helper string methods
+extension String {
+	var isInt: Bool {
+		return Int(self) != nil
+	}
+
+	var isOperator: Bool {
+		return self == "+" || self == "-" || self == "*" || self == "/"
+	}
+}
+
+// basic stack 
+struct Stack<T> {
+	var items = [T]()
+
+	mutating func push(_ item: T) {
+		items.append(item)
+	}
+
+	mutating func pop() -> T? {
+		return items.removeLast()
+	}
+
+	func last() -> T? {
+		return items.last
+	}
+}
+
+
+extension Array where Element == (String) {
+	
+	func evaluate() -> Int? {
+
+		var stack = Stack<Int>()
+
+		for item in self {
+
+			if item.isInt, let value = Int(item) {
+
+				stack.push(value)
+
+			} else if item.isOperator, let op1 = stack.pop(), let op2 = stack.pop() {
+
+				switch item {
+					case "+": stack.push(op1 + op2)
+					case "-": stack.push(op1 - op2)
+					case "*": stack.push(op1 * op2)
+					case "/": stack.push(op1 / op2)
+					default: return nil
+				}
+
+			} else {
+
+				return nil
+			}
+		}
+
+		return stack.pop()
+	}
+}
+
+let input = ["1", "2", "+"]
+print("Evaluating \(input) and result is: \(input.evaluate() ?? -1001)")
+
+let input2 = ["1", "2", "+", "4", "*"]
+print("Evaluating \(input2) and result is: \(input2.evaluate() ?? -1001)")
+
+let input3 = ["1", "2", "+", "5", "1", "*", "+"]
+print("Evaluating \(input3) and result is: \(input3.evaluate() ?? -1001)")
+
+let input4 = ["1", "2", "3", "+", "*"]
+print("Evaluating \(input4) and result is: \(input4.evaluate() ?? -1001)")
