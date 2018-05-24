46e21ca1be130337d098e8651acb25c5c50533e0
Jayahari Vavachan <jayahariv88@gmail.com>
2018-05-23 17:56:24 -0700

    fix: Word Ladder more test cases

--- swift/WordLadder.swift
+++ swift/WordLadder.swift
@@ -22,7 +22,17 @@
 
 	5. end when word is found or if queue is empty
 
+	NB: COMPLETE ASSUMPTION OF ONLY UTF8
+
 */
+
+extension String {
+	
+	// if you want to only check for next items in the dictionary
+	func isBefore(_ string: String) -> Bool {
+		return self < string
+	}
+}
 
 
 // This will be the dictionary of valid words.
@@ -38,7 +48,13 @@ extension Array where Element == (String) {
 			set.insert(item)
 		}
 
-		if from == end {
+		// GUARD: Make sure, start and end is not equal
+		guard from != end else {
+			return 1
+		}
+
+		// GUARD: Make sure end is in the dictionary
+		guard set.contains(end) else {
 			return 0
 		}
 		
@@ -64,7 +80,6 @@ extension Array where Element == (String) {
 
 				for char in "abcdefghijklmnopqrstuvwxyz" {
 
-					// ignore the same char
 					if word[index] != char {
 						let nextIndex = word.index(after: index)
 						newWord.replaceSubrange(index..<nextIndex, with: "\(char)")
@@ -77,12 +92,16 @@ extension Array where Element == (String) {
 				}
 			}
 
+			// GUARD: When no next elements found, its end
+			guard queue.size != 0 else {
+				return 0;
+			}
+
 
 			size -= 1
 			if size == 0 {
 				size = queue.size
 				distance += 1
-				print("======> \(distance) ==> \(queue.getItems())")
 			}
 		}
 
@@ -125,9 +144,11 @@ struct Queue<T> {
 	}
 }
 
-func test(_ start: String, _ end: String, _ dict: Array<String>) {
-	print("\nStart: \(start), End: \(end), Dict:\(dict) => Distance: \(dict.transformationDistance(start, to: end))")
+func test(_ start: String, _ end: String, _ dict: Array<String>, _ result: Int) {
+	print("Test => \(dict.transformationDistance(start, to: end) == result ? "Successful": "Failed")")
 }
 
-
-test("hit", "cog", ["cog", "hot","dot","dog","lot","log"])
+test("hot", "dog", ["hot","dog"], 0)
+test("hit", "cog", ["cog", "hot","dot","dog","lot","log"], 5)
+test("hit", "cog", ["hot","dot","dog","lot","log"], 0)
+test("cet", "ism", ["kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"], 11)
