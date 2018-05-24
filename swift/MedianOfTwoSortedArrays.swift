544718fb4f54f41be02760d3c417e882d1b885f9
Jayahari Vavachan <jayahariv88@gmail.com>
2018-05-24 00:16:23 -0700

    feat: Median non Overlapping logic added
    * seems like its wrong, my understanding on the problem is wrong.

--- swift/MedianOfTwoSortedArrays.swift
+++ swift/MedianOfTwoSortedArrays.swift
@@ -43,7 +43,70 @@ public extension Array where Element == (Int) {
 	// Call only if your array and the passed arrays are sorted
 
 	public func medianOf(_ array: Array<Element>) -> Double {
-		return 1.9
+
+		let total = (count + array.count)
+
+		// GUARD: Both array empty -> No result
+		guard total > 0 else {
+			return -1
+		}
+
+		// GUARD: If self is empty, take the median of given array
+		guard count > 0 else {
+			return array.median
+		}
+
+		// GUARD: If given array is empty, take the median of self
+		guard array.count > 0 else {
+			return self.median
+		}
+		
+		// NON OVERLAPPING -> median
+		
+		if self.last! < array.first! { 
+
+			// SELF then Array => O(1)
+
+			return nonOverlappingMedian(self, array)
+
+
+		} else if array.last! < self.first! {
+
+			// Array then SELF => O(1)
+
+			return nonOverlappingMedian(array, self)
+
+		} else { 
+
+			// OVERLAPPING
+
+			return 1.9
+
+		}
+
+	}
+
+	private func nonOverlappingMedian(_ array1: Array<Int>, _ array2: Array<Int>) -> Double {
+		
+		let total = array1.count + array2.count
+		let index = total/2
+
+		if total%2 != 0 { // ODD
+			if index < array1.count {
+				return Double(array1[index])
+			} else {
+				return Double(array2[index - array1.count])
+			}
+		} else { // EVEN
+			
+			if index + 1 < array1.count {
+				return Double(array1[index] + array1[index+1])/2.0
+			} else if index >= array1.count {
+				return Double(array2[index-array1.count] + array2[1+index-array1.count])/2.0
+			} else {
+				return Double(array1[index] + array2[1+index-array1.count])/2.0
+			}
+		}
 	}
 }
 
@@ -75,8 +138,16 @@ testMedianOfArray([1, 2, 3, 4], 2.5)
 // ------------------ Median Of 2 Arrays ------------------
 
 func test(_ array1: Array<Int>, _ array2: Array<Int>, _ result: Double) {
+	print(array1.medianOf(array2))
 	genTest("Median of 2 Arrays") { array1.medianOf(array2) == result }
 }
 
+// only one array
+test([], [5, 6, 7], 6.0)
+test([1, 5, 9], [], 5.0)
 
-test([1, 2], [3, 4], 1.9)
+// non overlapping
+test([1, 2, 3, 4], [5], 3.0)
+test([1], [5, 6, 7, 8], 6.0)
+test([1, 2, 3, 4], [5, 6, 7], 4.0)
+test([1, 20, 30], [50, 60, 70, 88], 50.0)
